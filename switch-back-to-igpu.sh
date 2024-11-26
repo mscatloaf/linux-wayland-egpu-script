76ea3538a2d3c72824bd4eeb1768eecf0ed115eb
catloaf <80300628+mscatloaf@users.noreply.github.com>
2024-11-26 12:42:30 -0800

    Added sudo to umount lines.
    This was needed to prevent an error about needing root permissions

--- switch-back-to-igpu.sh
+++ switch-back-to-igpu.sh
@@ -4,8 +4,8 @@ IGPU_PATH="/sys/bus/pci/devices/0000:00:02.0"
 EGPU_PATH="/sys/bus/pci/devices/0000:03:00.0"
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-umount $IGPU_PATH/boot_vga
-umount $EGPU_PATH/boot_vga
+sudo umount $IGPU_PATH/boot_vga
+sudo umount $EGPU_PATH/boot_vga
 sudo systemctl restart display-manager
 
 
