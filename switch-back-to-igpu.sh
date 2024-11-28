8b3a0793878c50b8c342ee6f8d036bfbbea185c9
catloaf <80300628+mscatloaf@users.noreply.github.com>
2024-11-27 17:31:13 -0800

    Update switch-back-to-igpu.sh
    Add warnings to switch-back-to-igpu.sh because it will most likely cause your system to lock up and delete any unsaved work.

--- switch-back-to-igpu.sh
+++ switch-back-to-igpu.sh
@@ -4,6 +4,10 @@ IGPU_PATH="/sys/bus/pci/devices/0000:00:02.0"
 EGPU_PATH="/sys/bus/pci/devices/0000:03:00.0"
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo WARNING, this script is likely to leave your system in an unusable state which requires a forced power off
+echo SAVE ANY UNSAVED WORK BEFORE RUNNING THIS SCRIPT
+read -p "press ENTER to continue"
+
 sudo umount $IGPU_PATH/boot_vga
 sudo umount $EGPU_PATH/boot_vga
 sudo systemctl restart display-manager
