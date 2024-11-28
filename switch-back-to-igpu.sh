99b126f76660abfb37464f18713ccd783cd01e7b
catloaf <80300628+mscatloaf@users.noreply.github.com>
2024-11-27 18:39:37 -0800

    Update switch-back-to-igpu.sh
    Commented line to remove amdgpu since it was unnecessary and worsened stability even on my intel system.

--- switch-back-to-igpu.sh
+++ switch-back-to-igpu.sh
@@ -12,11 +12,10 @@ SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #undo what the switch script did
 sudo umount $IGPU_PATH/boot_vga
 sudo umount $EGPU_PATH/boot_vga
-sleep 0.5
 
 #unbind the egpu
 echo -n $EGPU_PCI_ID | sudo tee /sys/bus/pci/drivers/amdgpu/unbind
-sudo rmmod amdgpu #this line is optional and should probably be commented out on amd laptops
+#sudo rmmod amdgpu #this line is optional and should probably be commented out on amd laptops
 sudo systemctl restart display-manager
 
 
